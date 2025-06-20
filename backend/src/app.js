7b753f87f4b84b6df2c93eba79d02ba1dbbd7ca7
nikp9 <nikhilparihar0908@gmail.com>
2025-06-20 22:11:00 +0530

    chore(init): setup db connections

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,11 +1,13 @@
 import express from 'express'
 import 'dotenv/config'
+import pool from './config/db.js'
 
 const app = express()
 const port = process.env.PORT
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.get('/', async(req, res) => {
+  const result = await pool.query("SELECT * FROM player_cricket_stats WHERE meets_min_criteria = true ORDER BY dynamic_strike_rate DESC LIMIT 50")
+  res.json(result.rows)
 })
 
 app.listen(port, () => {
---
+++ backend/src/config/db.js
@@ -0,0 +1,16 @@
+import {Pool} from 'pg'
+import 'dotenv/config'
+
+const pool = new Pool({
+    user: process.env.USER,
+    host: process.env.HOST,
+    database: process.env.DATABASE,
+    password: process.env.PASSWORD,
+    port: process.env.DBPORT
+})
+
+pool.on("connect", () => {
+    console.log("Database connection successful!")
+})
+
+export default pool
