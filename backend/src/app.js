8e5ebebe29ab9f4b62f363ec3fb6b674b6d64954
nikp9 <nikhilparihar0908@gmail.com>
2025-06-22 23:19:44 +0530

    feat(route): initialize player routes

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,14 +1,11 @@
 import express from 'express'
 import 'dotenv/config'
-import pool from './config/db.js'
+import playerRoutes from './routes/playerRoutes.js';
 
 const app = express()
 const port = process.env.PORT
 
-app.get('/', async(req, res) => {
-  const result = await pool.query("SELECT * FROM player_cricket_stats WHERE meets_min_criteria = true ORDER BY dynamic_strike_rate DESC LIMIT 50")
-  res.json(result.rows)
-})
+app.use('/api/players', playerRoutes);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
