1d55230a60994e96f570b898a5581bfeaa1bebc6
nikp9 <nikhilparihar0908@gmail.com>
2025-06-24 23:47:44 +0530

    chore(fix): minor fix in query

--- backend/src/models/playerModel.js
+++ backend/src/models/playerModel.js
@@ -3,7 +3,7 @@ import pool from '../config/db.js'
 const getPlayerStatsById = async (playerId) => {
   const query = `
     SELECT *
-    FROM player_cricket_stats
+    FROM player_cricket_stats_by_competition
     WHERE player_id = $1;
   `;
   const rows = await pool.query(query, [playerId]);
