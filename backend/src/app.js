fd4187a08b31f85b34e6dcc7c39e866c101249d6
nikp9 <nikhilparihar0908@gmail.com>
2025-07-01 22:20:22 +0530

    feat(routes): Update routes

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,11 +1,13 @@
 import express from 'express'
 import 'dotenv/config'
 import playerRoutes from './routes/playerRoutes.js';
+import searchRoute from './routes/searchRoute.js'
 
 const app = express()
 const port = process.env.PORT
 
 app.use('/api/player', playerRoutes);
+app.use('/api/searchList', searchRoute)
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
