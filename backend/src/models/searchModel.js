49942dac4aeb616813bf3e98f012adf5e6ad476f
nikhil <nikhilparihar0908@gmail.com>
2025-07-09 23:50:18 +0530

    feat(query): Update query params

--- backend/src/models/searchModel.js
+++ backend/src/models/searchModel.js
@@ -2,7 +2,7 @@ import pool from '../config/db.js'
 
 export const getSearchList = async () => {
   const query = `
-    select player_id, player_name, full_name, cricinfo_name, role, img_url from players
+    select player_id, player_name, full_name, cricinfo_name, role, country, gender, img_url from players
   `;
   const rows = await pool.query(query);
   return rows.rows;
