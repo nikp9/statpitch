aac09312d223b8ec584e7beaa0714032d103317f
nikhil <nikhilparihar0908@gmail.com>
2025-07-18 20:58:19 +0530

    feat(cors): Add root domain

--- backend/src/app.js
+++ backend/src/app.js
@@ -7,7 +7,25 @@ import cors from 'cors'
 const app = express()
 const port = process.env.PORT
 
-app.use(cors())
+const cors = require('cors');
+
+const allowedOrigins = [
+  'https://statpitch.com', 
+  'https://www.statpitch.com',
+  'http://localhost:3000',
+];
+
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      return callback(null, true);
+    } else {
+      return callback(new Error(`CORS not allowed for this origin: ${origin}`));
+    }
+  },
+  credentials: true
+}));
+
 
 app.use('/api/player', playerRoutes);
 app.use('/api/searchList', searchRoute)
