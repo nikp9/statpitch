02d4fe2c764a9e4544de828a2a8152cd2a25d6ff
nikhil <nikhilparihar0908@gmail.com>
2025-09-14 02:09:26 +0530

    load_balancer: Add go load balancer

---
+++ load_balancer/go.mod
@@ -0,0 +1,3 @@
+module github.com/nikp9/load_balancer
+
+go 1.25.1
---
+++ load_balancer/main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"crypto/tls"
+	"log"
+	"net/http"
+	"net/http/httputil"
+	"net/url"
+	"sync/atomic"
+)
+
+var backends = []string{
+	"http://127.0.0.1:3000",
+	// "http://127.0.0.1:3001",
+	// "http://127.0.0.1:3002",
+}
+
+var current uint32
+
+func pickNext() *url.URL {
+	i := atomic.AddUint32(&current, 1)
+	u, err := url.Parse(backends[(int(i)-1)%len(backends)])
+	if err != nil {
+		log.Fatalf("invalid backend URL: %v", err)
+	}
+	return u
+}
+
+func main() {
+	log.Println("Starting Go Load Balancer with TLS on :443")
+
+	handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		target := pickNext()
+		proxy := httputil.NewSingleHostReverseProxy(target)
+		proxy.ErrorHandler = func(w http.ResponseWriter, r *http.Request, e error) {
+			http.Error(w, "Upstream error: "+e.Error(), http.StatusBadGateway)
+		}
+		proxy.ServeHTTP(w, r)
+	})
+
+	server := &http.Server{
+		Addr:    ":443",
+		Handler: handler,
+		TLSConfig: &tls.Config{
+			MinVersion: tls.VersionTLS12,
+		},
+	}
+
+	certFile := "/etc/letsencrypt/live/api.statpitch.com/fullchain.pem"
+	keyFile := "/etc/letsencrypt/live/api.statpitch.com/privkey.pem"
+
+	log.Fatal(server.ListenAndServeTLS(certFile, keyFile))
+}
